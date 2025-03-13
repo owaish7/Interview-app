d5dd6d7ee5036647f93072bc19d2b12fea40dcdb
owaish7 <lightningowais@gmail.com>
2025-03-13 09:34:45 +0530

    authentication and routes done

--- react-interview-app/src/App.tsx
+++ react-interview-app/src/App.tsx
@@ -1,9 +1,34 @@
-
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom"
+import HomePage from "@/routes/home"
+import PublicLayout from "@/layouts/public-layout"
+import AuthenticationLayout from "@/layouts/auth-layouts";
+import SignInPage from "@/routes/sign-in";
+import SignUpPage from "@/routes/sign-up";
+import ProtectRoutes from "@/layouts/protected-routes";
+ import { MainLayout } from "@/layouts/main-layout";
+ 
 const App = () => {
   return (
-    <div className='text-blue-400'>
-      app
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<PublicLayout />} >
+        <Route index element={<HomePage/>}/>
+        </Route>
+        {/*authentication layout*/}
+        <Route element={<AuthenticationLayout />}>
+           <Route path="/signin/*" element={<SignInPage />} />
+           <Route path="/signup/*" element={<SignUpPage />} />
+         </Route>
+
+         <Route
+           element={
+             <ProtectRoutes>
+               <MainLayout />
+             </ProtectRoutes>
+           }
+         ></Route>
+      </Routes>
+    </Router>
   )
 }
 
