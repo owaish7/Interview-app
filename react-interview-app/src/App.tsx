e27ca38c0ae23c3755e810e61615ef28f8ad476c
owaish7 <lightningowais@gmail.com>
2025-03-14 01:44:23 +0530

    added all other functionality

--- react-interview-app/src/App.tsx
+++ react-interview-app/src/App.tsx
@@ -6,7 +6,14 @@ import SignInPage from "@/routes/sign-in";
 import SignUpPage from "@/routes/sign-up";
 import ProtectRoutes from "@/layouts/protected-routes";
  import { MainLayout } from "@/layouts/main-layout";
+ import { Dashboard } from "@/routes/dashboard";
+import { Generate } from "@/components/generate";
+import { CreateEditPage } from "@/routes/create-edit-page";
+import  {MockLoadPage}  from "@/routes/mock-load-page";
+import { MockInterviewPage } from "@/routes/mock-interview-page";
  
+import Feedback from "@/routes/feedback";
+
 const App = () => {
   return (
     <Router>
@@ -26,7 +33,22 @@ const App = () => {
                <MainLayout />
              </ProtectRoutes>
            }
-         ></Route>
+         >
+           <Route element={<Generate />} path="/generate">
+           
+           <Route index element={<Dashboard />} />
+           <Route path=":interviewId" element={<CreateEditPage />} />
+           <Route path="interview/:interviewId" element={<MockLoadPage />} />
+           <Route
+               path="interview/:interviewId/start"
+               element={<MockInterviewPage />}
+             />
+             <Route path="feedback/:interviewId" element={<Feedback />} />
+           </Route>
+
+
+
+         </Route>
       </Routes>
     </Router>
   )
--- react-interview-app/src/types/index.ts
+++ react-interview-app/src/types/index.ts
@@ -8,3 +8,17 @@ import { FieldValue, Timestamp } from "firebase/firestore";
    createdAt: Timestamp | FieldValue;
    updateAt: Timestamp | FieldValue;
  }
+
+
+ export interface Interview {
+  id: string;
+  position: string;
+  description: string;
+  experience: number;
+  userId: string;
+  techStack: string;
+  questions: { question: string; answer: string }[];
+  createdAt: Timestamp;
+  updateAt: Timestamp;
+}
+ 
