2f6ffa7c305c24c249d41e148ca04d4fbe202b97
owaish7 <lightningowais@gmail.com>
2025-03-13 16:09:12 +0530

    Revert "firebase done"
    This reverts commit 17ffc3fa23c345562679105bb3cc6a3ed12e989b.

--- react-interview-app/src/layouts/public-layout.tsx
+++ react-interview-app/src/layouts/public-layout.tsx
@@ -1,13 +1,11 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
-import AuthHandler from '@/handlers/auth-handler'
 import {Outlet} from "react-router-dom";
 
 const PublicLayout = () => {
   return (
     <div className='w-full'>
       { /* handler to store the user data*/}
-      <AuthHandler />
         <Header />
 
         <Outlet />
